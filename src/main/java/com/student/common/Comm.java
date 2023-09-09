320b89b637c0e6d6c33d66fc6a986069e71d6599
duylue <binhminhbenem2801@gmail.com>
2023-09-09 23:36:05 +0700

    first commit

--- src/main/java/com/student/common/Comm.java
+++ src/main/java/com/student/common/Comm.java
@@ -1,6 +1,8 @@
 package com.student.common;
 
 public class Comm {
+    public static String URL8088 = "http://192.168.2.13:8088";
+    public static String URI_LIST_PRODUCT_API = "/api/product";
     public static String MY_SQL_URL = "jdbc:mysql://localhost:3306/k12";
     public static String USERNAME_MYSQL = "root";
     public static String PASSWORD_MYSQL ="123456";
--- src/main/java/com/student/service/impl/ProductServiceImpl.java
+++ src/main/java/com/student/service/impl/ProductServiceImpl.java
@@ -1,22 +1,23 @@
 package com.student.service.impl;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.student.model.Category;
-import com.student.model.Product;
-import com.student.model.ProductDto;
-import com.student.model.Student;
+import com.student.base.BaseResponse;
+import com.student.common.Comm;
+import com.student.model.*;
 import com.student.repository.ProductRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.*;
 
 @Service
-public class ProductServiceImpl implements ProductService {
+public class ProductServiceImpl extends BaseResponse implements ProductService {
     private final EntityManagerFactory factory;
     @Autowired
     private ProductRepository productRepository;
@@ -55,10 +56,47 @@ public class ProductServiceImpl implements ProductService {
         ObjectMapper mapper = new ObjectMapper();
         List<ProductDto> products = new ArrayList<>();
         for (Map m : mapList) {
-            ProductDto dto = mapper.convertValue(m,ProductDto.class);
+            ProductDto dto = mapper.convertValue(m, ProductDto.class);
             products.add(dto);
         }
         return products;
+    }
+
+
+
+    @Override
+    public ResponseEntity<?> getListProductApiModel() {
+        WebClient webClient = WebClient.create(Comm.URL8088);
+        MyResponse myResponse = webClient.get()
+                .uri(Comm.URI_LIST_PRODUCT_API)
+                .retrieve()
+                .bodyToMono(MyResponse.class).block();
+        List<ProductAPI> apiList = (List<ProductAPI>) myResponse.getData();
+        System.out.println(apiList.get(0));
+        return getResponseEntity(myResponse.getData());
+    }
+
+    @Override
+    public ProductAPI getOne() {
+        WebClient webClient = WebClient.create(Comm.URL8088);
+        ProductAPI api = webClient.get()
+                .uri("/api/product/getOne")
+                .retrieve()
+                .bodyToMono(ProductAPI.class).block();
+
+        return api;
+    }
+
+    @Override
+    public ResponseEntity<?> postProductApi(ProductAPI api) {
+        WebClient client = WebClient.create(Comm.URL8088);
+        MyResponse myResponse = client.post()
+                .uri("/api/product/save")
+                .bodyValue(api)
+                .retrieve().bodyToMono(MyResponse.class)
+                .block();
+
+        return getResponseEntity(myResponse.getData());
     }
 
     @Override
